b79b5d20de9039be03c568b1e88c1fa854754271
Jason Costello <jason@jsnc.tl>
2023-10-29 21:30:37 -0400

    Using helm loader

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/jsnctl/steady
 
 go 1.21.0
+
+require helm.sh/helm/v3 v3.13.1
+
+require (
+	github.com/Masterminds/semver/v3 v3.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
--- steady.go
+++ steady.go
@@ -3,12 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"helm.sh/helm/v3/pkg/chart/loader"
 )
 
 const DefaultChart = "charts/example"
 
 func main() {
-	cmd := flag.String("chart", DefaultChart, "Select chart directory")
+	valuesFile := flag.String("chart", DefaultChart, "Select chart directory")
 	flag.Parse()
-	fmt.Printf(*cmd)
+
+	chart, err := loader.Load(*valuesFile)
+
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+
+	for _, f := range chart.Raw {
+		fmt.Printf("    |    \n")
+		fmt.Printf("|" + f.Name + "|\n")
+		fmt.Printf("    |    \n")
+	}
 }
