008b5377cadc59c354c12d9c0b424d6cb777ebad
Jason Costello <jason@jsnc.tl>
2023-11-15 02:59:34 -0500

    Some test refactoring

--- draw/node_test.go
+++ draw/node_test.go
@@ -5,22 +5,40 @@ import (
 	"testing"
 )
 
+type Tree struct {
+	nodes map[string]*Node
+}
+
+/*
+		 A
+		/ \
+	   B   C
+			\
+			 D
+*/
+func testTree() *Tree {
+	nodeMap := make(map[string]*Node)
+	nodes := []string{"A", "B", "C", "D", "E"}
+	for _, node := range nodes {
+		nodeMap[node] = &Node{Name: node}
+	}
+
+	tree := &Tree{nodes: nodeMap}
+
+	tree.nodes["A"].AddChild(tree.nodes["B"])
+	tree.nodes["A"].AddChild(tree.nodes["C"])
+	tree.nodes["C"].AddChild(tree.nodes["D"])
+
+	return tree
+}
 func TestNode(t *testing.T) {
-	A := Node{Name: "A"}
-	B := Node{Name: "B"}
-	C := Node{Name: "C"}
-	D := Node{Name: "D"}
-	E := Node{Name: "E"}
+	tree := testTree()
 
-	A.AddChild(&B)
-	A.AddChild(&C)
-	C.AddChild(&D)
+	assert.Equal(t, 2, len(tree.nodes["A"].Children))
+	assert.Equal(t, 1, len(tree.nodes["C"].Children))
 
-	assert.Equal(t, 2, len(A.Children))
-	assert.Equal(t, 1, len(C.Children))
-
-	assert.True(t, IsNodeInTree(&A, &D))
-	assert.True(t, IsNodeInTree(&A, &C))
-	assert.False(t, IsNodeInTree(&A, &E))
-	assert.False(t, IsNodeInTree(&C, &A))
+	assert.True(t, IsNodeInTree(tree.nodes["A"], tree.nodes["D"]))
+	assert.True(t, IsNodeInTree(tree.nodes["A"], tree.nodes["C"]))
+	assert.False(t, IsNodeInTree(tree.nodes["A"], tree.nodes["E"]))
+	assert.False(t, IsNodeInTree(tree.nodes["C"], tree.nodes["A"]))
 }
